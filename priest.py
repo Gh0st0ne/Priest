354fe1194edfff0490f5a034ece35bc5ab2369d0
whoami021 <whoamiclay021@gmail.com>
2021-01-22 14:33:14 +0330

    commit

--- priest.py
+++ priest.py
@@ -4,47 +4,36 @@ The Priest WebServer https://github.com/sinsinsecurity/Priest
 Usage::
     ./server.py [<port>]
 """
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
 
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+#!/usr/bin/env python3
+
+import http.server as SimpleHTTPServer
+import socketserver as SocketServer
+import logging
+import sys
+
+if(len(sys.argv) < 2):
+    print("[!] No port specified, using default port :8000")
+    PORT=8000
+else:
+    PORT = int(sys.argv[1])
+
+
+
+print("[*] the Priest is listening on address http://0.0.0.0:%d" % PORT)
+
+class GetHandler(
+        SimpleHTTPServer.SimpleHTTPRequestHandler
+        ):
 
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        logging.error(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+Handler = GetHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-def run(server_class=HTTPServer, handler_class=S, port=8000):
-    logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+httpd.serve_forever()
 
-if __name__ == '__main__':
-    from sys import argv
 
-    if len(argv) == 2:
-        print("[*] the Priest is listening on address http://0.0.0.0:" + argv[1])
-        run(port=int(argv[1]))
-    else:
-        print("[*] the Priest is listening on address http://0.0.0.0:8000")
-        run()
